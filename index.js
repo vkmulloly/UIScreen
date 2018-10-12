f0b4a3138b8bf78e61c0c5f40688a7653a8dafde
Vineeth <30654527+vkmulloly@users.noreply.github.com>
2018-10-12 20:45:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { AppRegistry } from 'react-native';
+import ChatScreen from './App/Container/ChatScreen';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => ChatScreen);
